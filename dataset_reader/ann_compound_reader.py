b2c00d305458424427475d089226b67eeee63c5a
Garth-brick <i-vagarth.dvivedi@devrev.ai>
2024-07-16 14:15:46 +0000

    added patch to load multiple vector files

--- dataset_reader/ann_compound_reader.py
+++ dataset_reader/ann_compound_reader.py
@@ -1,4 +1,6 @@
 import json
+import os
+import re
 from typing import Iterator, List
 
 import numpy as np
@@ -15,14 +17,26 @@ class AnnCompoundReader(JSONReader):
     """
 
     VECTORS_FILE = "vectors.npy"
+    VECTORS_FILE_RE = re.compile("^vectors.*.npy$")
     QUERIES_FILE = "tests.jsonl"
 
     def read_vectors(self) -> Iterator[List[float]]:
-        vectors = np.load(self.path / self.VECTORS_FILE)
-        for vector in vectors:
-            if self.normalize:
-                vector = vector / np.linalg.norm(vector)
-            yield vector.tolist()
+        file_names = []
+        for file_name in os.listdir(self.path):
+            if not self.VECTORS_FILE_RE.match(file_name):
+                continue
+            file_names.append(file_name)
+        file_names.sort()
+        n = len(file_names)
+        print(f"Uploading vectors from {n} files: {file_names}")
+        for xx, file_name in enumerate(file_names):
+            print(f"Uploading vectors from file {xx + 1}/{n}: {file_name}")
+            vectors = np.load(self.path / file_name)
+            for vector in vectors:
+                if self.normalize:
+                    vector = vector / np.linalg.norm(vector)
+                yield vector.tolist()
+
 
     def read_queries(self) -> Iterator[Query]:
         with open(self.path / self.QUERIES_FILE) as payloads_fp:
